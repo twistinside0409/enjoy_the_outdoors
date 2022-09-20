f84541c8a2dddab3507b6a7671c31438a310df77
Alessandri <Elizabeth.Alessandri@LibertyMutual.com>
2022-09-20 12:14:10 -0400

    mountain dropdown populates

--- assets/scripts/load_mountains_data.js
+++ assets/scripts/load_mountains_data.js
@@ -2,21 +2,29 @@
 
 let mountainsArray = []
 
-//My add
-let mountainList = document.getElementById("#mountain-list") //"DDL"
-let mountainOutput = document.getElementById("#mountain-output") //"outputdiv"
 
 
 window.onload = function(){
 
     loadJsonData("assets/data/mountains.json").then((mountains) => {
         mountainsArray = mountains.mountains;
+        const mountainList = document.getElementById('mountain-list');
+        let arrLength = mountainsArray.length;
+        for (let i = 0; i < arrLength; i++) {
+            let mountains = new Option(mountainsArray[i].name);
+            mountainList.appendChild(mountains);
+        }
     })
 
 }
 
 
+
+
+
+
 //function that can "fetch" the sunset/sunrise times
+
 let loadJsonData = async (path) => {
     let response = await fetch(path)
     let data = await response.json()
